79635297106bb34eb3f0b01ea3a1bc2f92b1d2ba
godwinn22 <godwinohazulike@gmail.com>
2024-09-28 17:06:35 +0100

    0. Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,16 @@
 
 
 def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of int): A list of lists representing the grid,
+                                    where 0 represents water and 1 represents land.
+
+    Returns:
+        int: The perimeter of the island.
+    """
     perimeter = 0
 
     for row in range(len(grid)):
